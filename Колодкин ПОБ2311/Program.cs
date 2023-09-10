c74bb2e72f9bac31e3ab82c85bd978eee7639041
Пользователь <@DESKTOP-2GPIOM1>
2023-09-11 00:59:15 +0300

    Добавьте файлы проекта.

---
+++ Колодкин ПОБ2311/Program.cs
@@ -0,0 +1,157 @@
+﻿////16
+//Console.WriteLine("Введите y:");
+//double y=double.Parse(Console.ReadLine());
+//Console.WriteLine("Решение!");
+//double S = Math.Sqrt(Math.Cos(4 * y * y + 7.151));
+//Console.WriteLine($"S={S}");
+
+////17
+//Console.WriteLine("Введите y");
+//double y= double.Parse(Console.ReadLine());
+//Console.WriteLine("Решение!");
+//double N = 3 * y * y + Math.Sqrt(y + 1);
+//Console.WriteLine($"N={N}");
+
+//18
+//Console.WriteLine("Введение y:");
+//double y= double.Parse(Console.ReadLine());
+//Console.WriteLine("Решение!");
+//double Z = 3 * y * y + Math.Sqrt(y * y * y + 1);
+//Console.WriteLine($"Z={Z}");
+
+//19
+//Console.WriteLine("Введите y:");
+//double y= double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите n:");
+//double n= double.Parse(Console.ReadLine()); 
+//Console.WriteLine("Решение!");
+//double P = Math.Sqrt(y * y * y + 1.09);
+//Console.WriteLine($"P={P}");
+
+//20
+//Console.WriteLine("Введите y:");
+//double y=double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите x");
+//double x=double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите e:");
+//double e=double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите k:");
+//double k=double.Parse(Console.ReadLine());  
+//Console.WriteLine("Решение!");
+//double U = e * k + y + Math.Tan(x)*(Math.Sqrt(y));
+//Console.WriteLine($"U={U}");
+
+////21
+//Console.WriteLine("Введите e:");
+//double e= double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите y:");
+//double y= double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите h:");
+//double h= double.Parse(Console.ReadLine());
+//Console.WriteLine("Решение!");
+//double P = e * y + 5.5 + (9.1 * h * h * h);
+//Console.WriteLine($"P={P}");
+
+
+////22
+//Console.WriteLine("Введите u:");
+//double u= double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите y:");
+//double y= double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите x:");
+//double x= double.Parse(Console.ReadLine());
+//Console.WriteLine("Решение!");
+//double T = Math.Sin(2 * u) * Math.Log(2 * y * y + (Math.Abs(x)));
+//Console.WriteLine($"T={T}");
+
+////23
+//Console.WriteLine("Введите e:");
+//double e= double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите y:");
+//double y= double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите f:");
+//double f= double.Parse(Console.ReadLine());
+//Console.WriteLine("Решение!");
+//double G = e * e * y + Math.Sin(f);
+//Console.WriteLine($"G={G}");
+
+////24
+//Console.WriteLine("Введите y:");
+//double y= double.Parse(Console.ReadLine());
+//Console.WriteLine("Решение!");
+//double F = 2 * Math.Sin(0.214 * y * y * y * y * y + 1);
+//Console.WriteLine($"F={F}");
+
+////25
+//Console.WriteLine("Введите e:");
+//double e= double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите y:");
+//double y= double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите f");
+//double f= double.Parse(Console.ReadLine());
+//Console.WriteLine("Решение!");
+//double G = e * e * y + Math.Sin(f * f);
+//Console.WriteLine($"G={G}");
+
+////26
+////Console.WriteLine("Введите p");
+////double p= double.Parse(Console.ReadLine());
+////Console.WriteLine("Введите Решение!");
+////double Z = Math.Sin(p * p + 0.4*3);
+////Console.WriteLine($"Z={Z}");
+
+
+////27
+//Console.WriteLine("Введите v:");
+//double v=double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите e:");
+//double e=double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите y:");
+//double y=double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите x:");
+//double x=double.Parse(Console.ReadLine());
+//Console.WriteLine("Рещение!");
+//double W = 1.03 * v + e * e * y + Math.Tan(Math.Abs(x));
+//Console.WriteLine($"W={W}");
+
+////28
+//Console.WriteLine("Введите e:");
+//double e= double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите y:");
+//double y= double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите h:");
+//double h= double.Parse(Console.ReadLine());
+//Console.WriteLine("Решение!");
+//double T = (e * y + h) + Math.Sqrt(Math.Abs(6.4 * y));
+//Console.WriteLine($"T={T}");
+
+
+
+////29
+//Console.WriteLine("Введите y:");
+//double y=double.Parse(Console.ReadLine());
+//Console.WriteLine("Решение!");
+//double N = 3 * y * y + Math.Sqrt(Math.Abs(y + 1));
+//Console.WriteLine($"N={N}");
+
+////30
+//Console.WriteLine("Введите e:");
+//double e= double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите y:");
+//double y= double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите r:");
+//double r= double.Parse(Console.ReadLine());
+//Console.WriteLine("Решение!");
+//double W = e * y + r + (7.2 * Math.Sin(r));
+//Console.WriteLine($"W={W}");
+
+
+
+
+
+
+
+
+
+
+
